ae3bf350134afe3c169409be843da9abbb30742a
HuyPhan1232203 <154007180+HuyPhan1232203@users.noreply.github.com>
2025-10-24 16:54:35 +0700

    new

--- script.js
+++ script.js
@@ -1,231 +1,94 @@
-class Tool {
-  static randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+var c = document.getElementById("alx");
 
-  static randomColorRGB() {
-    return `rgb(${this.randomNumber(0, 255)}, ${this.randomNumber(
-      0,
-      255
-    )}, ${this.randomNumber(0, 255)})`;
-  }
+var b = document.body;
+var a = c.getContext("2d");
 
-  static randomColorHSL(hue, saturation, lightness) {
-    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-  }
+e = [];
+h = [];
+WIDTH = c.width = innerWidth;
+HEIGHT = c.height = innerHeight;
+v = 32 + 16 + 8;
+R = Math.random;
+C = Math.cos;
+Y = 6.3;
 
-  static gradientColor(ctx, cr, cg, cb, ca, x, y, r) {
-    const col = `${cr},${cg},${cb}`;
-    const g = ctx.createRadialGradient(x, y, 0, x, y, r);
-    g.addColorStop(0, `rgba(${col}, ${ca * 1})`);
-    g.addColorStop(0.5, `rgba(${col}, ${ca * 0.5})`);
-    g.addColorStop(1, `rgba(${col}, ${ca * 0})`);
-    return g;
-  }
-}
+for (i = 0; i < Y; i += 0.2)
+  h.push([
+    WIDTH / 2 + 210 * Math.pow(Math.sin(i), 3),
+    HEIGHT / 2 + 13 * -(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)),
+  ]);
 
-class Angle {
-  constructor(a) {
-    this.a = a;
-    this.rad = (this.a * Math.PI) / 180;
-  }
+for (i = 0; i < Y; i += 0.4)
+  h.push([
+    WIDTH / 2 + 150 * Math.pow(Math.sin(i), 3),
+    HEIGHT / 2 + 9 * -(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)),
+  ]);
 
-  incDec(num) {
-    this.a += num;
-    this.rad = (this.a * Math.PI) / 180;
-  }
-}
+for (i = 0; i < Y; i += 0.8)
+  h.push([
+    WIDTH / 2 + 90 * Math.pow(Math.sin(i), 3),
+    HEIGHT / 2 + 5 * -(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)),
+  ]);
 
-let canvas;
-let offCanvas;
-
-class Canvas {
-  constructor(bool) {
-    this.canvas = document.createElement("canvas");
-    if (bool === true) {
-      this.canvas.style.position = "relative";
-      this.canvas.style.display = "block";
-      this.canvas.style.top = 0;
-      this.canvas.style.left = 0;
-      document.body.appendChild(this.canvas);
-    }
-    this.ctx = this.canvas.getContext("2d");
-    this.width = this.canvas.width = window.innerWidth;
-    this.height = this.canvas.height = window.innerHeight;
-    this.width < 768 ? (this.heartSize = 180) : (this.heartSize = 250);
-    this.mouseX = null;
-    this.mouseY = null;
-    this.hearts = [];
-    this.offHeartNum = 1;
-    this.offHearts = [];
-    this.data = null;
-  }
-
-  onInit() {
-    let index = 0;
-    for (let i = 0; i < this.height; i += 12) {
-      for (let j = 0; j < this.width; j += 12) {
-        let oI = (j + i * this.width) * 4 + 3;
-        if (this.data[oI] > 0) {
-          index++;
-          const h = new Heart(
-            canvas.ctx,
-            j + Tool.randomNumber(-3, 3),
-            i + Tool.randomNumber(-3, 3),
-            Tool.randomNumber(6, 12),
-            index
-          );
-          canvas.hearts.push(h);
-        }
-      }
-    }
-  }
-
-  offInit() {
-    for (let i = 0; i < this.offHeartNum; i++) {
-      const s = new Heart(
-        this.ctx,
-        this.width / 2,
-        this.height / 2.3,
-        this.heartSize
-      );
-      this.offHearts.push(s);
-    }
-    for (let i = 0; i < this.offHearts.length; i++) {
-      this.offHearts[i].offRender(i);
-    }
-    this.data = this.ctx.getImageData(0, 0, this.width, this.height).data;
-    this.onInit();
-  }
-
-  render() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
-    for (let i = 0; i < this.hearts.length; i++) {
-      this.hearts[i].render(i);
-    }
-  }
-
-  resize() {
-    this.offHearts = [];
-    this.hearts = [];
-    this.width = this.canvas.width = window.innerWidth;
-    this.height = this.canvas.height = window.innerHeight;
-    this.width < 768 ? (this.heartSize = 180) : (this.heartSize = 250);
-  }
-}
-
-class Heart {
-  constructor(ctx, x, y, r, i) {
-    this.ctx = ctx;
-    this.init(x, y, r, i);
-  }
-
-  init(x, y, r, i) {
-    this.x = x;
-    this.xi = x;
-    this.y = y;
-    this.yi = y;
-    this.r = r;
-    this.i = i * 0.5 + 200;
-    this.l = this.i;
-    this.c = Tool.randomColorHSL(Tool.randomNumber(-5, 5), 80, 60);
-    this.a = new Angle(Tool.randomNumber(0, 360));
-    this.v = {
-      x: Math.random(),
-      y: -Math.random(),
+for (i = 0; i < v; ) {
+  x = R() * WIDTH;
+  y = R() * HEIGHT;
+  H = 80 * (i / v) + Math.random * 100;
+  S = 40 * R() + 60;
+  B = 60 * R() + 20;
+  f = [];
+  for (k = 0; k < v; )
+    f[k++] = {
+      x: x,
+      y: y,
+      X: 0,
+      Y: 0,
+      R: 1 - k / v + 1,
+      S: R() + 1,
+      q: ~~(R() * v),
+      D: 2 * (i % 2) - 1,
+      F: 0.2 * R() + 0.7,
+      f: "hsla(" + ~~H + "," + ~~S + "%," + ~~B + "%,.1)",
     };
-    this.ga = Math.random();
-  }
-
-  draw() {
-    const ctx = this.ctx;
-    ctx.save();
-    ctx.globalCompositeOperation = "lighter";
-    ctx.globalAlpha = this.ga;
-    ctx.beginPath();
-    ctx.fillStyle = this.c;
-    ctx.moveTo(this.x, this.y + this.r);
-    ctx.bezierCurveTo(
-      this.x - this.r - this.r / 5,
-      this.y + this.r / 1.5,
-      this.x - this.r,
-      this.y - this.r,
-      this.x,
-      this.y - this.r / 5
-    );
-    ctx.bezierCurveTo(
-      this.x + this.r,
-      this.y - this.r,
-      this.x + this.r + this.r / 5,
-      this.y + this.r / 1.5,
-      this.x,
-      this.y + this.r
-    );
-    ctx.closePath();
-    ctx.fill();
-    ctx.restore();
-  }
-
-  updateParams() {
-    this.a.incDec(1);
-    Math.sin(this.a.rad) < 0
-      ? (this.r = -Math.sin(this.a.rad) * 20)
-      : (this.r = Math.sin(this.a.rad) * 20);
-  }
-
-  updatePosition() {
-    this.l -= 1;
-    if (this.l < 0) {
-      this.v.y -= 0.01;
-      this.v.x += 0.02;
-      this.y += this.v.y;
-      this.x += this.v.x;
-    }
-  }
-
-  wrapPosition() {
-    if (this.x > canvas.width * 1.5) {
-      this.init(this.xi, this.yi, Tool.randomNumber(6, 12), this.i);
-    }
-  }
-
-  render() {
-    this.wrapPosition();
-    this.updateParams();
-    this.updatePosition();
-    this.draw();
-  }
-
-  offRender(i) {
-    this.draw();
-  }
+  e[i++] = f;
 }
 
-(function () {
-  "use strict";
-  window.addEventListener("load", function () {
-    offCanvas = new Canvas(false);
-    canvas = new Canvas(true);
-
-    offCanvas.offInit();
-
-    function render() {
-      window.requestAnimationFrame(function () {
-        canvas.render();
-        render();
-      });
-    }
-
-    render();
-
-    window.addEventListener(
-      "resize",
-      function () {
-        canvas.resize();
-        offCanvas.resize();
-        offCanvas.offInit();
-      },
-      false
-    );
-  });
-})();
+function path(d) {
+  a.fillStyle = d.f;
+  a.beginPath();
+  a.arc(d.x, d.y, d.R, 0, Y, 1);
+  a.closePath();
+  a.fill();
+}
+setInterval(function () {
+  a.fillStyle = "rgba(0,0,0,.2)";
+  a.fillRect(0, 0, WIDTH, HEIGHT);
+  for (i = v; i--; ) {
+    f = e[i];
+    u = f[0];
+    q = h[u.q];
+    D = u.x - q[0];
+    E = u.y - q[1];
+    G = Math.sqrt(D * D + E * E);
+    10 > G &&
+      (0.95 < R()
+        ? (u.q = ~~(R() * v))
+        : (0.99 < R() && (u.D *= -1),
+          (u.q += u.D),
+          (u.q %= v),
+          0 > u.q && (u.q += v)));
+    u.X += (-D / G) * u.S;
+    u.Y += (-E / G) * u.S;
+    u.x += u.X;
+    u.y += u.Y;
+    path(u);
+    u.X *= u.F;
+    u.Y *= u.F;
+    for (k = 0; k < v - 1; )
+      (T = f[k]),
+        (N = f[++k]),
+        (N.x -= 0.7 * (N.x - T.x)),
+        (N.y -= 0.7 * (N.y - T.y)),
+        path(N);
+  }
+}, 25);
